9564a257c0f55a733dfe92c3daf4de29e87c992b
Samana Shrestha <samana0shrestha@gmail.com>
2024-09-23 13:03:30 +0545

    navbar_1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,10 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import logo from'../assets/img/logo.svg';
+import navIcon1 from'../assets/img/nav-icon1.svg';
+import navIcon2 from'../assets/img/navIcon2.svg';
+import navIcon3 from'../assets/img/navIcon3.svg';
+
 import { useState, useEffect} from 'react';
 export const NavBar = () => {
     const [activeLink, setActiveLink]= useState('home');
@@ -15,10 +20,13 @@ export const NavBar = () => {
         window.addEventListener("scroll",onScroll);
         return()=>window.removeEventListener("scroll",onScroll);
     },[])
+    const onUpdateActiveLink=(value)=>{
+      setActiveLink(value);
+    }
     return(
         <Navbar expand="lg" className={scrolled?"scrolled":""}>
       <Container>
-        <Navbar.Brand href="#home"><img src={''} alt="logo"/></Navbar.Brand>
+        <Navbar.Brand href="#home"><img src={logo} alt="logo"/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <span className="navbar-toggler-icon"/>
         <Navbar.Collapse id="basic-navbar-nav">
@@ -29,9 +37,9 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-            <a href="#"><img src={} alt=""/></a>
-            <a href="#"><img src={} alt=""/></a>
-            <a href="#"><img src={} alt=""/></a>
+            <a href="#"><img src={navIcon1} alt=""/></a>
+            <a href="#"><img src={navIcon2} alt=""/></a>
+            <a href="#"><img src={navIcon3} alt=""/></a>
             </div>
             <button className="vvd" onClick={() => console.log('connect')}><span> Let's Connect</span></button>
           </span>
